7e36a6f7e216f0ad465bb497f0e4c9ce6eacec2d
Simon Hoyos <shmesa23@gmail.com>
2019-10-31 11:14:14 -0500

    Update tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,15 +2,30 @@ import React from 'react';
 import App from './App';
 import configureStore from 'redux-mock-store';
 import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import toJson from 'enzyme-to-json';
 
-const mockStore = configureStore();
+const middlewares = [thunk]
+const mockStore = configureStore(middlewares);
 
 it('renders without crashing', () => {
   const increase = jest.fn();
   const decrease = jest.fn();
-  const store = mockStore({ increase, decrease, countReducer: { count: 0 } });
+  const getPosts = jest.fn();
+  const store = mockStore({
+    increase,
+    decrease,
+    getPosts,
+    countReducer: { count: 0 },
+    postsReducer: { posts: []}
+  });
 
-  const wrapper = mount(<App store={store} />);
+  const wrapper = mount(
+    <Provider store={store}>
+      <App store={store} />
+    </Provider>
+  );
 
-  expect(wrapper).toMatchSnapshot();
+  expect(toJson(wrapper)).toMatchSnapshot();
 });
